62ae1e3cbc2895fd49cc5f720ef94374bad8c763
nkemakolam <nkemakolamnnadi3@gmail.com>
2019-03-21 12:27:04 +0100

    update heroku file

--- app/view.py
+++ app/view.py
@@ -6,6 +6,6 @@ from app import process_data
 @app.route('/',methods = ['GET'])
 
 def index():
-    return render_template("index.html", data = process_data.process())
+    return render_template("index.html", data = process_data.processpython ())
 
 
